e83887aa932073d27c94f2f576ae10e459ac1051
Kasparas Mickevičius <termissues@gmail.com>
2026-09-22 02:40:19 +0300

    Delete old case name folder

--- Assignment01_variables/task3.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-let fiveCents = Number(prompt("Kiek penkių centų monetų yra taupyklėje?"));
-let twentyCents = Number(prompt("Kiek dvidešimties centų monetų yra taupyklėje?"));
-let twoLitas = Number(prompt("Kiek dviejų litų monetų yra taupyklėje?"));
-let moneySaved = (fiveCents * 0.05) + (twentyCents * 0.20) + (twoLitas * 2);
-alert(`Taupyklėje yra ${moneySaved} litų.`);
